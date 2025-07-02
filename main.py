7161f32a199a5f782990fcce0e28c431a0f07127
tmp-lemon <fnleonar@protonmail.com>
2025-07-02 07:09:54 +0000

    update workflow

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os, sys
 from dotenv import load_dotenv
+
 from google import genai
 from google.genai import types
+
+from available_functions import available_functions
+from system_prompt import system_prompt
 
 
 def main():
@@ -12,46 +16,20 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-
-    model_name = "gemini-2.0-flash-001"
+    model = "gemini-2.0-flash-001"
     
     user_prompt = sys.argv[1]
+    verbose = len(sys.argv) > 2 and sys.argv[2] == "--verbose"
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    system_prompt = """
-    You are a helpful AI coding agent.
+    generate_content(client, model, messages, user_prompt, verbose)
 
-    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
-    - List files and directories
-
-    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-    """
-    
-    schema_get_files_info = types.FunctionDeclaration(
-        name="get_files_info",
-        description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
-        parameters=types.Schema(
-            type=types.Type.OBJECT,
-            properties={
-                "directory": types.Schema(
-                    type=types.Type.STRING,
-                    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
-                ),
-            },
-        ),
-    )
-
-    available_functions = types.Tool(
-        function_declarations=[
-            schema_get_files_info,
-        ]
-    )
-
+def generate_content(client, model, messages, user_prompt, verbose):
     response = client.models.generate_content(
-        model=model_name, 
+        model=model, 
         contents=messages,
         config=types.GenerateContentConfig(
             tools=[available_functions],
@@ -59,19 +37,16 @@ def main():
         )
     )
 
+    if verbose:
+        print("User prompt:", user_prompt)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)    
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+
     if not response.function_calls:
         return response.text
-
+    
     for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-
-    if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
-        prompt_tokens = response.usage_metadata.prompt_token_count
-        response_tokens = response.usage_metadata.candidates_token_count
-        
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {prompt_tokens}")    
-        print(f"Response tokens: {response_tokens}")    
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")  
 
 
 if __name__ == "__main__":
